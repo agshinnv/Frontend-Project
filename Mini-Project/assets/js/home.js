e8a16df0616dd08074b60eb472354810cd8c7f33
Agshin N. Valiyev <agshinnv@code.edu.az>
2024-03-23 02:02:40 +0400

    header completed

--- Mini-Project/assets/js/home.js
+++ Mini-Project/assets/js/home.js
@@ -4,11 +4,41 @@ $(function(){
     $(".right-side li .content").click(function(){
         $(this).next().toggleClass("content-menu-down");
         $(this).parent().siblings().find(".content-menu").removeClass("content-menu-down");
-    })
+    });
+
+    $(".searching .selecting").click(function () {
+        $(this).children().first().toggleClass("rotate");
+        $(this).next().toggleClass("search-down");
+        $(this).parent().siblings().find(".selecting-menu").removeClass("search-down");
+    });
+    
+    $(".all-categories").click(function(){
+        $(".categories").toggleClass("d-none")
+    });
+
+
+    // $(".products-icon").hover(function(e){
+    //     e.preventDefault(e);
+    //     $(".additional-navbar").toggleClass("d-none")
+    // })
+
+
+    $(".products-icon").mouseover(function(e){
+        e.preventDefault(e);
+        $(".additional-navbar").addClass("open");
+        $(".additional-navbar").removeClass("d-none");
+        $(".additional-navbar").removeClass("close");
+
+    });
+
+    $(".products-icon").mouseout(function(e){
+        e.preventDefault(e);
+        $(".additional-navbar").removeClass("open");
+        $(".additional-navbar").addClass("d-none");
+        $(".additional-navbar").addClass("close");
+    });
+    
+
+
 });
 
-$(".searching .selecting").click(function () {
-    $(this).children().first().toggleClass("rotate");
-    $(this).next().toggleClass("search-down");
-    $(this).parent().siblings().find(".selecting-menu").removeClass("search-down");
-  });
