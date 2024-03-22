01de5be7ad83ef05c2282fbeb1a84ad313f0013e
Agshin N. Valiyev <agshinnv@code.edu.az>
2024-03-22 17:07:18 +0400

    header-middle added

--- Mini-Project/assets/js/home.js
+++ Mini-Project/assets/js/home.js
@@ -5,4 +5,10 @@ $(function(){
         $(this).next().toggleClass("content-menu-down");
         $(this).parent().siblings().find(".content-menu").removeClass("content-menu-down");
     })
-})
+});
+
+$(".searching .selecting").click(function () {
+    $(this).children().first().toggleClass("rotate");
+    $(this).next().toggleClass("search-down");
+    $(this).parent().siblings().find(".selecting-menu").removeClass("search-down");
+  });
